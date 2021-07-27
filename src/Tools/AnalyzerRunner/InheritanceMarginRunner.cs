eb0a180848acb1447e5f26b20216958bac5f4cc0
Shen Chen <shech@microsoft.com>
2021-07-27 12:27:52 -0700

    Init comment

---
+++ src/Tools/AnalyzerRunner/InheritanceMarginRunner.cs
@@ -0,0 +1,41 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Shared.Extensions;
+using Microsoft.CodeAnalysis.InheritanceMargin;
+
+namespace AnalyzerRunner
+{
+    public class InheritanceMarginRunner
+    {
+        private readonly Workspace _workspace;
+
+        public InheritanceMarginRunner(Workspace workspace)
+        {
+            _workspace = workspace;
+        }
+
+        public async Task RunAsync(CancellationToken cancellationToken)
+        {
+            var allProjects = _workspace.CurrentSolution.Projects;
+            var collector = new Dictionary<Project, string>();
+
+            foreach (var project in allProjects)
+            {
+                var allDocuments = project.Documents.ToArray();
+                foreach (var document in allDocuments)
+                {
+                    var inheritanceMarginService = document.GetLanguageService<IInheritanceMarginService>();
+                }
+            }
+        }
+    }
+}
