9f143de3976449213674ae133a3cdcc2e6563ba9
Shen Chen <cos_ifne@hotmail.com>
2021-08-13 19:15:05 -0700

    Find the right point to place context menu

--- src/VisualStudio/Core/Def/Implementation/InheritanceMargin/InheritanceMarginCommandHandler.cs
+++ src/VisualStudio/Core/Def/Implementation/InheritanceMargin/InheritanceMarginCommandHandler.cs
@@ -5,21 +5,16 @@
 using System;
 using System.Collections.Immutable;
 using System.ComponentModel.Composition;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using Microsoft.CodeAnalysis.Editor;
 using Microsoft.CodeAnalysis.Editor.Shared.Extensions;
 using Microsoft.CodeAnalysis.Host.Mef;
-using Microsoft.CodeAnalysis.InheritanceMargin;
 using Microsoft.VisualStudio.Commanding;
-using Microsoft.VisualStudio.Debugger.ComponentInterfaces;
 using Microsoft.VisualStudio.Editor;
-using Microsoft.VisualStudio.LanguageServices.Implementation.InheritanceMargin.MarginGlyph;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Formatting;
 using Microsoft.VisualStudio.Text.Tagging;
 using Microsoft.VisualStudio.TextManager.Interop;
 using Microsoft.VisualStudio.Utilities;
@@ -44,7 +39,22 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.InheritanceMarg
 
         public bool ExecuteCommand(InheritanceMarginCommandArgs args, CommandExecutionContext executionContext)
         {
-            if (!TryGetInheritanceMarginTag(args, out var tag))
+            var textView = args.TextView;
+            var textBuffer = args.SubjectBuffer;
+            var caret = textView.GetCaretPoint(textBuffer);
+            if (caret == null)
+            {
+                return false;
+            }
+
+            var caretLine = textView.TextViewLines.GetTextViewLineContainingBufferPosition(caret.Value);
+            if (caretLine == null)
+            {
+                return false;
+            }
+
+            var tag = GetInheritanceMarginTag(textView, caretLine);
+            if (tag == null)
             {
                 return false;
             }
@@ -56,14 +66,41 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.InheritanceMarg
             }
 
             var glyphMargin = wpfTextViewHost.GetTextViewMargin(PredefinedMarginNames.Glyph);
+            if (glyphMargin == null)
+            {
+                return false;
+            }
 
-            return false;
+            var marginGrid = glyphMargin.VisualElement;
+            var xAxisPosition = marginGrid.ActualWidth / 2 + textView.ViewportLeft;
+            var yAxisPosition = textView.ViewportTop + (caretLine.TextTop + caretLine.TextBottom) / 2;
+            // TODO: this should be the real context menu
+            var contextMenu = new ContextMenu();
+            contextMenu.PlacementTarget = marginGrid;
+            contextMenu.Placement = PlacementMode.RelativePoint;
+            contextMenu.HorizontalOffset = xAxisPosition;
+            contextMenu.VerticalOffset = yAxisPosition;
+            contextMenu.IsOpen = true;
+
+            return true;
         }
 
         public CommandState GetCommandState(InheritanceMarginCommandArgs args)
-            => TryGetInheritanceMarginTag(args, out var _) ? CommandState.Available : CommandState.Unavailable;
+            => CommandState.Available;
 
-        private IWpfTextViewHost? GetMarginTextViewHost()
+        private InheritanceMarginTag? GetInheritanceMarginTag(ITextView view, ITextViewLine textViewLine)
+        {
+            var tagAggregator = _aggregatorFactoryService.CreateTagAggregator<InheritanceMarginTag>(view);
+            var tags = tagAggregator.GetTags(new SnapshotSpan(textViewLine.Start, length: 0)).ToImmutableArray();
+            if (tags.Length != 1)
+            {
+                return null;
+            }
+
+            return tags[0].Tag;
+        }
+
+        private static IWpfTextViewHost? GetMarginTextViewHost()
         {
             var vsTextManager = (IVsTextManager)Shell.Package.GetGlobalService(typeof(SVsTextManager));
             var getActiveViewResult = vsTextManager.GetActiveView(fMustHaveFocus: 1, null, out var vsTestView);
@@ -77,30 +114,6 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.InheritanceMarg
             }
 
             return null;
-        }
-
-        private bool TryGetInheritanceMarginTag(InheritanceMarginCommandArgs args, [NotNullWhen(true)] out InheritanceMarginTag? tag)
-        {
-            var textView = args.TextView;
-            var caret = textView.GetCaretPoint(args.SubjectBuffer);
-            if (caret == null)
-            {
-                tag = null;
-                return false;
-            }
-
-            var caretLine = textView.TextViewLines.GetTextViewLineContainingBufferPosition(caret.Value);
-
-            var tagAggregator = _aggregatorFactoryService.CreateTagAggregator<InheritanceMarginTag>(textView);
-            var tags = tagAggregator.GetTags(new SnapshotSpan(caretLine.Start, length: 0)).ToImmutableArray();
-            if (tags.Length != 1)
-            {
-                tag = null;
-                return false;
-            }
-
-            tag = tags[0].Tag;
-            return true;
         }
     }
 }
