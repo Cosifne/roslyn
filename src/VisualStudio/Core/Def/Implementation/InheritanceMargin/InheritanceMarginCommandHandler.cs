aeb0daf46346f18cbbcccadedf8ff89fc9e7db86
Shen Chen <shech@microsoft.com>
2021-08-13 17:59:29 -0700

    Let command handler can find wpfTextViewHost

--- src/VisualStudio/Core/Def/Implementation/InheritanceMargin/InheritanceMarginCommandHandler.cs
+++ src/VisualStudio/Core/Def/Implementation/InheritanceMargin/InheritanceMarginCommandHandler.cs
@@ -6,15 +6,22 @@ using System;
 using System.Collections.Immutable;
 using System.ComponentModel.Composition;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
 using Microsoft.CodeAnalysis.Editor;
 using Microsoft.CodeAnalysis.Editor.Shared.Extensions;
 using Microsoft.CodeAnalysis.Host.Mef;
 using Microsoft.CodeAnalysis.InheritanceMargin;
 using Microsoft.VisualStudio.Commanding;
 using Microsoft.VisualStudio.Debugger.ComponentInterfaces;
+using Microsoft.VisualStudio.Editor;
+using Microsoft.VisualStudio.LanguageServices.Implementation.InheritanceMargin.MarginGlyph;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.TextManager.Interop;
 using Microsoft.VisualStudio.Utilities;
 
 namespace Microsoft.VisualStudio.LanguageServices.Implementation.InheritanceMargin
@@ -37,17 +44,40 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.InheritanceMarg
 
         public bool ExecuteCommand(InheritanceMarginCommandArgs args, CommandExecutionContext executionContext)
         {
-            if (TryGetInheritanceMarginTag(args, out var tag))
+            if (!TryGetInheritanceMarginTag(args, out var tag))
             {
-                // TODO: Use tag to open the margin
-                return true;
+                return false;
             }
+
+            var wpfTextViewHost = GetMarginTextViewHost();
+            if (wpfTextViewHost == null)
+            {
+                return false;
+            }
+
+            var glyphMargin = wpfTextViewHost.GetTextViewMargin(PredefinedMarginNames.Glyph);
 
             return false;
         }
 
         public CommandState GetCommandState(InheritanceMarginCommandArgs args)
             => TryGetInheritanceMarginTag(args, out var _) ? CommandState.Available : CommandState.Unavailable;
+
+        private IWpfTextViewHost? GetMarginTextViewHost()
+        {
+            var vsTextManager = (IVsTextManager)Shell.Package.GetGlobalService(typeof(SVsTextManager));
+            var getActiveViewResult = vsTextManager.GetActiveView(fMustHaveFocus: 1, null, out var vsTestView);
+            if (getActiveViewResult == VSConstants.S_OK && vsTestView is IVsUserData vsUserData)
+            {
+                var getDataResult = vsUserData.GetData(DefGuidList.guidIWpfTextViewHost, out var pvtData);
+                if (getDataResult == VSConstants.S_OK && pvtData is IWpfTextViewHost wpfTextViewHost)
+                {
+                    return wpfTextViewHost;
+                }
+            }
+
+            return null;
+        }
 
         private bool TryGetInheritanceMarginTag(InheritanceMarginCommandArgs args, [NotNullWhen(true)] out InheritanceMarginTag? tag)
         {
