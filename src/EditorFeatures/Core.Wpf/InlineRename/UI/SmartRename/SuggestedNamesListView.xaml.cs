3702668ec3e99dfc9bae3715de555509f0c6342f
Shen Chen <shech@microsoft.com>
2023-10-17 23:07:22 -0700

    Add the list view to sperate control

---
+++ src/EditorFeatures/Core.Wpf/InlineRename/UI/SmartRename/SuggestedNamesListView.xaml.cs
@@ -0,0 +1,33 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Microsoft.CodeAnalysis.InlineRename.UI.SmartRename
+{
+    /// <summary>
+    /// Interaction logic for SuggestedNamesControl.xaml
+    /// </summary>
+    internal partial class SuggestedNamesListView : ListView
+    {
+        public static readonly DependencyProperty IsExpandedProperty =
+            DependencyProperty.Register(
+                name: nameof(IsExpanded),
+                propertyType: typeof(bool),
+                ownerType: typeof(SuggestedNamesListView),
+                typeMetadata: new FrameworkPropertyMetadata(defaultValue: false));
+
+        public bool IsExpanded
+        {
+            get => (bool)GetValue(IsExpandedProperty);
+            set => SetValue(IsExpandedProperty, value);
+        }
+
+        public SuggestedNamesListView()
+        {
+            InitializeComponent();
+        }
+    }
+}
