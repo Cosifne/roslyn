592b98ebeaf64d6264a5ef0a2218fbf24faec8b7
Shen Chen <shech@microsoft.com>
2023-10-27 16:11:50 -0700

    Cover the assert for catalog

--- src/VisualStudio/Core/Test.Next/Options/UnifiedSettingsTests.cs
+++ src/VisualStudio/Core/Test.Next/Options/UnifiedSettingsTests.cs
@@ -2,25 +2,25 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
-using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
-using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Options;
 using Microsoft.CodeAnalysis.SolutionCrawler;
+using Microsoft.VisualStudio.LanguageServices;
+using Microsoft.VisualStudio.PlatformUI;
 using Xunit;
 
 namespace Roslyn.VisualStudio.Next.UnitTests.Options
 {
     public class UnifiedSettingsTests
     {
-        private readonly ImmutableArray<IOption2> _migratedOptionsInAdvancedOptionPage = ImmutableArray.Create<IOption2>(
+        private static readonly ImmutableArray<IOption2> s_migratedOptionsInAdvancedOptionPage = ImmutableArray.Create<IOption2>(
             SolutionCrawlerOptionsStorage.BackgroundAnalysisScopeOption,
             SolutionCrawlerOptionsStorage.CompilerDiagnosticsScopeOption);
+        private const string CSharpAdvancedCatalogName = "textEditor.c#.advanced";
 
         [Fact]
         public async Task TestAdvancedUnifiedSettings()
@@ -28,8 +28,40 @@ namespace Roslyn.VisualStudio.Next.UnitTests.Options
             var assembly = typeof(UnifiedSettingsTests).Assembly;
             var registrationFileName = typeof(UnifiedSettingsTests).Assembly.GetManifestResourceNames().Single(name => name.EndsWith("csharpAdvancedSettings.registration.json"));
             using var fileStream = assembly.GetManifestResourceStream(registrationFileName);
-            var reader = new StreamReader(fileStream);
-            var file = await reader.ReadToEndAsync();
+            using var document = await JsonDocument.ParseAsync(fileStream);
+            foreach (var jsonProperty in document.RootElement.EnumerateObject())
+            {
+                if (jsonProperty.Name is "properties")
+                {
+                    VerifyProperties(jsonProperty.Value, CSharpAdvancedCatalogName, s_migratedOptionsInAdvancedOptionPage));
+                }
+                else if (jsonProperty.Name is CSharpAdvancedCatalogName)
+                {
+                    VerifyCatalog(jsonProperty.Value, Guids.CSharpOptionPageAdvancedIdString);
+                }
+                else
+                {
+                    Assert.True(false, "Unexpected element in the Unified Settings Json");
+                }
+            }
+        }
+
+        private static void VerifyProperties(JsonElement jsonElement, string expectedCatalog, ImmutableArray<IOption2> expectedOptions)
+        {
+            var allSettingsInJson = jsonElement.EnumerateObject();
+
+        }
+
+        private static void VerifyCatalog(JsonElement unifiedSettingCatalog, string legacyOptionPageId)
+        {
+            if (unifiedSettingCatalog.TryGetProperty("legacyOptionPageId", out var value))
+            {
+                Assert.Equal(legacyOptionPageId, value.ToString());
+            }
+            else
+            {
+                Assert.True(false, "Unexpected element in the Unified Settings Json");
+            }
         }
     }
 }
